c30d842eb183ac3a55b19fb5653df74f7f2af292
codewithhuzaifa <huzaifaarab2002@gmail.com>
2025-03-11 09:08:12 +0500

    Refactor BelugaOutlined button to support social icons and improve padding defaults; remove unused social button files

--- lib/package/components/buttons/beluga_outlined.dart
+++ lib/package/components/buttons/beluga_outlined.dart
@@ -1,130 +1,4 @@
-import 'package:beluga_design_flutter/package/components/buttons/beluga_elevated.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-// // class CustomOutlineButton extends StatelessWidget {
-// //   final String text;
-// //   final VoidCallback? onPressed;
-// //   final Color? backgroundColor;
-// //   final Color borderColor;
-// //   final Color? textColor;
-// //   final double borderWidth;
-// //   final double? fontSize;
-// //   final double borderRadius;
-// //   final EdgeInsetsGeometry? padding;
-// //   final double? width;
-// //   final double? height;
-// //   const CustomOutlineButton(
-// //       {super.key,
-// //       this.backgroundColor,
-// //       this.borderColor = Colors.blue,
-// //       this.text = "button",
-// //       this.onPressed,
-// //       this.textColor,
-// //       this.borderWidth = 1.0,
-// //       this.fontSize,
-// //       this.borderRadius = 3.0,
-// //       this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-// //       this.width,
-// //       this.height});
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Container(
-// //       width: width,
-// //       height: height,
-// //       decoration: BoxDecoration(
-// //         color: backgroundColor,
-// //         border: Border.all(color: borderColor, width: borderWidth),
-// //         borderRadius: BorderRadius.circular(borderRadius),
-// //       ),
-// //       child: OutlinedButton(
-// //         onPressed: onPressed,
-// //         style: ButtonStyle(
-// //           padding: WidgetStateProperty.all<EdgeInsetsGeometry>(padding!),
-// //           foregroundColor: WidgetStateProperty.all<Color>(
-// //             textColor!,
-// //           ),
-// //           side: WidgetStateProperty.all(BorderSide.none),
-// //           shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-// //             RoundedRectangleBorder(
-// //               borderRadius: BorderRadius.circular(borderRadius),
-// //             ),
-// //           ),
-// //         ),
-// //         child: Text(
-// //           text,
-// //           style: TextStyle(fontSize: fontSize, color: textColor),
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
-
-// import 'package:flutter/material.dart';
-
-// class CustomOutlineButton extends StatelessWidget {
-//   final String text;
-//   final VoidCallback? onPressed;
-//   final Color? backgroundColor;
-//   final Color borderColor;
-//   final Color? textColor;
-//   final double borderWidth;
-//   final double? fontSize;
-//   final double borderRadius;
-//   final EdgeInsetsGeometry? padding;
-//   final double? width;
-//   final double? height;
-
-//   const CustomOutlineButton({
-//     super.key,
-//     this.backgroundColor,
-//     this.borderColor = Colors.blue,
-//     this.text = "button",
-//     this.onPressed,
-//     this.textColor,
-//     this.borderWidth = 1.0,
-//     this.fontSize,
-//     this.borderRadius = 3.0,
-//     this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//     this.width,
-//     this.height,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: width,
-//       height: height,
-//       // decoration: BoxDecoration(
-//       //   color: backgroundColor,
-//       //   border: Border.all(color: borderColor, width: borderWidth),
-//       //   borderRadius: BorderRadius.circular(borderRadius),
-//       // ),
-//       child: OutlinedButton(
-//         onPressed: onPressed,
-//         style: ButtonStyle(
-//           padding: WidgetStateProperty.all(
-//               padding), // Use MaterialStateProperty.all directly
-//           foregroundColor: WidgetStateProperty.all(
-//               textColor ?? Colors.black), // Set a default text color if null
-//           side: WidgetStateProperty.all(BorderSide.), // No border side
-//           shape: WidgetStateProperty.all(
-//             RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(borderRadius),
-//             ),
-//           ),
-//         ),
-//         child: Text(
-//           text,
-//           style: TextStyle(
-//               fontSize: fontSize ?? 14,
-//               color: textColor ?? Colors.black), // Use default values if null
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:beluga_design_flutter/beluga_design.dart';
 
 class BelugaOutlined extends StatelessWidget {
   final String text;
@@ -134,7 +8,7 @@ class BelugaOutlined extends StatelessWidget {
   final double fontSize;
   final double borderWidth;
   final double borderRadius;
-  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry padding;
   final IconData? icon;
   final double iconSize;
   final double? width;
@@ -143,29 +17,28 @@ class BelugaOutlined extends StatelessWidget {
   final ButtonStyle? style;
   final bool autofocus;
   final TextStyle? textStyle;
-  final String? imagePath;
-  final bool showSocial;
+  final SocialIcons? socialIcon; // Enum se image ya icon set hoga
 
-  const BelugaOutlined(
-      {super.key,
-      this.text = "Button CTA",
-      this.onPressed,
-      this.borderColor = const Color(0xFF6A35FC), // Default Purple Color
-      this.textColor = const Color(0xFF6A35FC),
-      this.fontSize = 14.0,
-      this.borderWidth = 2.0,
-      this.borderRadius = 30.0, // High radius for pill shape
-      this.padding,
-      this.icon,
-      this.iconSize = 20.0,
-      this.width,
-      this.height,
-      this.child,
-      this.style,
-      this.autofocus = false,
-      this.textStyle,
-      this.imagePath,
-      this.showSocial = false});
+  const BelugaOutlined({
+    super.key,
+    this.text = "Button CTA",
+    this.onPressed,
+    this.borderColor = const Color(0xFF6A35FC),
+    this.textColor = const Color(0xFF6A35FC),
+    this.fontSize = 16.0,
+    this.borderWidth = 2.0,
+    this.borderRadius = 30.0,
+    this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+    this.icon, // Normal Icon ke liye
+    this.iconSize = 20.0,
+    this.width,
+    this.height,
+    this.child,
+    this.style,
+    this.autofocus = false,
+    this.textStyle,
+    this.socialIcon, // SVG image ke liye
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -173,8 +46,7 @@ class BelugaOutlined extends StatelessWidget {
       onPressed: onPressed ?? () {},
       style: style ??
           OutlinedButton.styleFrom(
-            padding:
-                padding ?? EdgeInsets.symmetric(horizontal: 20, vertical: 12.h),
+            padding: padding,
             side: BorderSide(
               color: borderColor,
               width: borderWidth,
@@ -184,26 +56,38 @@ class BelugaOutlined extends StatelessWidget {
             ),
           ),
       autofocus: autofocus,
-      child: child ??
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (icon != null) ...[
-                Icon(icon, color: textColor, size: iconSize),
-                const SizedBox(width: 8),
-              ],
-              Text(
-                text,
-                style: textStyle ??
-                    TextStyle(
-                      fontSize: fontSize.sp,
-                      color: textColor,
-                      fontWeight: FontWeight.w500,
-                    ),
-              ),
-            ],
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (socialIcon != null) ...[
+            // Agar socialIcon diya ho
+            SvgPicture.asset(socialIcon!.path, width: 20, height: 20),
+            const SizedBox(width: 8),
+          ] else if (icon != null) ...[
+            // Agar sirf normal icon diya ho
+            Icon(icon, color: textColor, size: iconSize),
+            const SizedBox(width: 8),
+          ],
+          Text(
+            text,
+            style: textStyle ??
+                TextStyle(
+                  fontSize: fontSize,
+                  color: textColor,
+                  fontWeight: FontWeight.w500,
+                ),
           ),
+        ],
+      ),
     );
   }
+}
+
+enum SocialIcons {
+  google("assets/image/social.svg"),
+  facebook("assets/image/lightfb.svg"),
+  apple("assets/image/apple.svg");
+
+  final String path;
+  const SocialIcons(this.path);
 }
